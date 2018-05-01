23ddfecbf1802fe87724c3af6e9125d55469e2d2
Yuliya204 <s01051640@acad.tri-c.edu>
2018-04-30 21:03:03 -0400

    lab 8 final commit

---
+++ InClassExamples/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+  <ul>
+    <li>some text</li>
+    <li>text</li>
+    <li>more text</li>
+  </ul>
+               );
+ReactDOM.render(
+  myList, 
+  document.getElementById('app')
+);
